099cdb2af1ce9913bc26f2ceb39d19f5ead45efb
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-06 11:54:44 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,110 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [12, 8],
+            "answer": 4
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": [14, 21],
+            "answer": 7
+        },
+        {
+            "input": [13, 11],
+            "answer": 1
+        },
+        {
+            "input": [55, 66],
+            "answer": 11
+        },
+        {
+            "input": [50, 10],
+            "answer": 10
+        },
+        {
+            "input": [29, 37],
+            "answer": 1
+        },
+        {
+            "input": [94, 47],
+            "answer": 47
+        },
+        {
+            "input": [65, 125],
+            "answer": 5
+        },
+        {
+            "input": [36, 156],
+            "answer": 12
+        },
+        {
+            "input": [36, 9],
+            "answer": 9
+        },
+        {
+            "input": [8, 32],
+            "answer": 8
+        },
+        {
+            "input": [17, 17],
+            "answer": 17
+        },
+        {
+            "input": [88, 24],
+            "answer": 8
         }
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": [12, 8],
+            "answer": 4
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": [14, 21],
+            "answer": 7
+        },
+        {
+            "input": [13, 11],
+            "answer": 1
+        },
+        {
+            "input": [55, 66],
+            "answer": 11
+        },
+        {
+            "input": [50, 10],
+            "answer": 10
+        },
+        {
+            "input": [29, 37],
+            "answer": 1
+        },
+        {
+            "input": [94, 47],
+            "answer": 47
+        },
+        {
+            "input": [65, 125],
+            "answer": 5
+        },
+        {
+            "input": [36, 156],
+            "answer": 12
+        },
+        {
+            "input": [36, 9],
+            "answer": 9
+        },
+        {
+            "input": [8, 32],
+            "answer": 8
+        },
+        {
+            "input": [17, 17],
+            "answer": 17
+        },
+        {
+            "input": [88, 24],
+            "answer": 8
         }
     ]
 }
